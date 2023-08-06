0334cca2b65ad55f9d46df359f6ebeb4d8d7c7a2
Salith Bin Anwar <salithanwar69@gmail.com>
2023-08-06 15:57:56 +0600

    errorMiddleware added to the server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from '../backend/routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
-
 const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.use('/api/users', userRoutes);
 
