c4e0562fd6c3d07fe2afcae1ecd0622af8e62d2a
Salith Bin Anwar <salithanwar69@gmail.com>
2023-08-06 18:20:31 +0600

    db added to the server

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from '../backend/routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
 app.use('/api/users', userRoutes);
+
+connectDB();
 
 app.use(notFound);
 app.use(errorHandler);
