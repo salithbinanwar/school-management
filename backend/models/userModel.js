63f741af2e1c4e3b65917b7c654c7edd29d44c5c
Salith Bin Anwar <salithanwar69@gmail.com>
2023-08-06 19:20:44 +0600

    model added

---
+++ backend/models/userModel.js
@@ -0,0 +1,23 @@
+const userSchema = mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true,
+        },
+        email:{
+            type: String,
+            required: true,
+        },
+        password:{
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
